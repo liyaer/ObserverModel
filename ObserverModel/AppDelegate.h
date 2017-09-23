2b9ab618ff93d76c77c795f182a03242cd4a525b
DuWenLiang <739285514@qq.com>
2017-09-23 09:15:39 +0800

    Initial Commit

---
+++ ObserverModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObserverModel
+//
+//  Created by 杜文亮 on 2017/9/23.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
